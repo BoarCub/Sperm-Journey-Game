0f54b0276818349d6ae52d6a592f9a78be4ec8f8
Unknown <dsanyal@live.com>
2019-06-17 04:12:57 -0700

    Added Uterus

---
+++ Sperm Journey/Assets/Leukocyte.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leukocyte : MonoBehaviour
+{
+
+    GameObject player;
+    private Rigidbody2D rb;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(rb.transform.position, player.transform.position, speed * Time.deltaTime);
+        rb.transform.position = newPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<UterusGame>().reduceHealth();
+        }
+    }
+
+}
---
+++ Sperm Journey/Assets/UterusGame.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UterusGame : MonoBehaviour
+{
+    public float health = 100;
+    public float healthRate = 0.5f;
+
+    private Rigidbody2D rb;
+
+    public TMPro.TextMeshProUGUI text;
+
+    public GameObject gameOver;
+
+    public GameObject win;
+
+    public AudioClip death;
+    public AudioClip winClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        text.text = "Health: 100/100";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            Win();
+        }
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Win()
+    {
+        AudioSource audioSource = FindObjectOfType<AudioSource>();
+        audioSource.clip = winClip;
+        audioSource.Play();
+        audioSource.loop = false;
+        win.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GameOver()
+    {
+        AudioSource audioSource = FindObjectOfType<AudioSource>();
+        audioSource.clip = death;
+        audioSource.Play();
+        audioSource.loop = false;
+        gameOver.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void reduceHealth()
+    {
+        health -= healthRate * Time.deltaTime;
+
+        if (health < 0)
+        {
+            health = 0;
+            GameOver();
+        }
+
+        text.text = "Health: " + (int)(health) + "/100";
+
+    }
+
+}
